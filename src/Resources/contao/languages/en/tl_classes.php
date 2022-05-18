cb272236acef2322be2b2e40ca53a9509ae3aca4
Martin Schittig <martin@dieschittigs.de>
2022-05-18 18:56:55 +0200

    tl_classes translations

---
+++ src/Resources/contao/languages/en/tl_classes.php
@@ -0,0 +1,16 @@
+<?php
+$GLOBALS['TL_LANG']['tl_classes']['title_legend'] = 'CSS-Classes-Settings';
+$GLOBALS['TL_LANG']['tl_classes']['show_legend'] = 'Usage-Settings';
+$GLOBALS['TL_LANG']['tl_classes'][0] = 'CSS-Classes';
+$GLOBALS['TL_LANG']['tl_classes'][1] = 'Define CSS classes and their use';
+$GLOBALS['TL_LANG']['tl_classes']['name'][0] = 'Name';
+$GLOBALS['TL_LANG']['tl_classes']['name'][1] = 'Input the name of the class. Only used in the backend';
+$GLOBALS['TL_LANG']['tl_classes']['cssClass'][0] = 'CSS-Class';
+$GLOBALS['TL_LANG']['tl_classes']['cssClass'][1] = 'Enter your classname (without the .)';
+$GLOBALS['TL_LANG']['tl_classes']['showOnPage'][0] = 'Use on pages';
+$GLOBALS['TL_LANG']['tl_classes']['showOnPage'][1] = 'This class can be used in the page settings';
+$GLOBALS['TL_LANG']['tl_classes']['showOnArticle'][0] = 'Use on articles';
+$GLOBALS['TL_LANG']['tl_classes']['showOnArticle'][1] = 'This class can be used in article settings';
+$GLOBALS['TL_LANG']['tl_classes']['showOnElement'][0] = 'Use on elements';
+$GLOBALS['TL_LANG']['tl_classes']['showOnElement'][1] = 'This class can be used in element settings';
+
